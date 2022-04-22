5f192ae3f407b96ef8edad14e6898fb1653277ec
brandoluu <brandonluu9@gmail.com>
2022-04-22 13:42:54 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -39,7 +39,8 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
+            if (!markdown.substring(currentIndex,markdown.length()).contains("[")
+            || !markdown.substring(currentIndex,markdown.length()).contains("]")) {
                 return toReturn;
             }
         
